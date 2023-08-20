8a8f4224db5a12df85a395cd9109020dad5243d8
Cam <61366617+cjens00@users.noreply.github.com>
2023-08-20 02:30:02 -0700

    Print String on Line 8 now Uses Correct Terminology
    Changed the print string on line 8 to now use the correct terminology.

--- cs/CXXInterop/Program.cs
+++ cs/CXXInterop/Program.cs
@@ -5,7 +5,7 @@ class Program {
     [DllImport("demolibrary.dll")]
     static extern int add_two_numbers(int a, int b);
     public static void Main(String[] args) {
-        Console.WriteLine("Adding two numbers using an unmanaged function (native) compiled from C/CXX.");
+        Console.WriteLine("Adding two numbers using an unmanaged function (native) compiled from CXX.");
         int result = add_two_numbers(50, 75);
         Console.WriteLine($"50 + 75 = {result}");
     }
